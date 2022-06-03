84c938c6854271eb54fe63402b492c22676931a6
Juan Rodriguez <Juan.Rodriguezh@bizagi.com>
2022-06-03 00:54:18 -0500

    Add project files.

---
+++ Context/ProductContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace WebApplication1.Context
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Models.Product> Products { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name ="Product Name")]
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+    }
+}
