d7637afe65806ed6f8b91db73e98342d0c831ba0
Juan Rodriguez <Juan.Rodriguezh@bizagi.com>
2022-06-03 02:18:31 -0500

    Updating EF Datasource Server and controller reference

--- Context/ProductContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.Entity;
-
-namespace WebApplication1.Context
-{
-    public class ProductContext : DbContext
-    {
-        public DbSet<Models.Product> Products { get; set; }
-    }
-}
